dc43703f2eaeb8494423cf437dcc0b9f4e97416d
Weiming-Hu <cosmos.weiming@outlook.com>
2018-12-02 16:08:01 -0500

    update

--- scripts/final/profile-parallel-v2.sh
+++ scripts/final/profile-parallel-v2.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-time OMP_NUM_THREADS=1 mpirun -np 1 parallelJacobi2 ../../data/ncdf4/1600.nc 10000 3 0 ;
-} 2>> log1.txt
+echo 1 >> log-fix-size.txt
+time mpirun -np 1 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 2 >> log-fix-size.txt
+time mpirun -np 2 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 4 >> log-fix-size.txt
+time mpirun -np 4 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 8 >> log-fix-size.txt
+time mpirun -np 8 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 16 >> log-fix-size.txt
+time mpirun -np 16 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 32 >> log-fix-size.txt
+time mpirun -np 32 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 64 >> log-fix-size.txt
+time mpirun -np 64 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
+echo 100 >> log-fix-size.txt
+time mpirun -np 100 parallelJacobi2 ../../data/ncdf4/1600.nc 90000 3 0 >> log-fix-size.txt
+
